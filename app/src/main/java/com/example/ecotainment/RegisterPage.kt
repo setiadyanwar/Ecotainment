6f03ee02f2eec91cd6653d403bb26aa5a93574a6
setiadyanwar <setiadyanwar@gmail.com>
2024-10-31 17:20:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ecotainment/RegisterPage.kt
@@ -0,0 +1,11 @@
+package com.example.ecotainment
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterPage : AppCompatActivity() {
+
+}
