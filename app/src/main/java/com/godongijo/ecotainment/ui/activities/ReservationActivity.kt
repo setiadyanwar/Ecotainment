20af31d79f15a34eadbb85d0126c1dae87f3c22b
Muhammad Farhan Fahrezy <113013896+farhanfahrezyy@users.noreply.github.com>
2024-12-03 19:54:58 +0700

    Update ReservationActivity.kt

--- app/src/main/java/com/godongijo/ecotainment/ui/activities/ReservationActivity.kt
+++ app/src/main/java/com/godongijo/ecotainment/ui/activities/ReservationActivity.kt
@@ -6,16 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.godongijo.ecotainment.R
+import com.godongijo.ecotainment.databinding.ActivityGotoSchoolBinding
+import com.godongijo.ecotainment.databinding.ActivityReservationBinding
 
 class ReservationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityReservationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_reservation)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityReservationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListeners()
+
+
+
+    }
+
+    private fun setListeners() {
+        binding.backButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
         }
     }
 }
